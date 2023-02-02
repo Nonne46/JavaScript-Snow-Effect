e1aaed38bc58dd36f846f121774d84791c05a3ca
nullneso <42183296+Nonne46@users.noreply.github.com>
2023-02-03 01:15:59 +0200

    minor changes

--- snoweffect.js
+++ snoweffect.js
@@ -1,8 +1,3 @@
-// -------------------------------------------------------------------------------------------------
-// JavaScript Snow Effect - © Copyright 2020 - Jam-Es.com
-// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
-// -------------------------------------------------------------------------------------------------
-
 // Change the following variables to customize the appearance of the particles
 var numParticles = window.innerWidth / 3;
 var maxSpeed = 2.5;
@@ -16,69 +11,95 @@ var canvasId = "snowcanvas";
 // If the following is true, the canvas resolution will be set to match the full viewport width and height.
 var fixCanvasResolution = true;
 
-var imageArray = ['q1.png', 'q2.png', 'q3.png', 'q4.png', 'q5.png'];
+var imageArray = [
+  "q1.webp",
+  "q2.webp",
+  "q3.webp",
+  "q4.webp",
+  "q5.webp",
+  "q6.webp",
+  "q7.webp",
+  "q8.webp",
+  "q9.webp",
+  "q10.webp",
+  "q11.webp",
+  "q12.webp",
+  "q13.webp",
+  "q14.webp",
+  "q15.webp",
+  "q16.webp",
+  "q17.webp",
+];
 var images = [];
 
 var snow_particles = [];
 
-window.onload = function() {
-	for (var i = 0; i < imageArray.length; i++) {
-	  var image = new Image();
-	  image.src = 'images/' + imageArray[i];
-	  images.push(image);
-	}
+window.onload = function () {
+  for (var i = 0; i < imageArray.length; i++) {
+    var image = new Image();
+    image.src = "images/" + imageArray[i];
+    images.push(image);
+  }
 
+  if (fixCanvasResolution) {
+    var c = document.getElementById(canvasId);
+    c.width = window.innerWidth;
+    c.height = window.innerHeight;
+  }
 
-	if (fixCanvasResolution) {
-		var c = document.getElementById(canvasId);
-		c.width = window.innerWidth;
-		c.height = window.innerHeight;
-	}
+  InitPoints();
 
-	InitPoints();
-
-	setInterval(Redraw, 5);
-}
+  setInterval(Redraw, 5);
+};
 
 function Redraw() {
-	var c = document.getElementById(canvasId);
-	var ctx = c.getContext("2d");
-	ctx.clearRect(0,0,c.width,c.height);
-	for (i = 0; i < snow_particles.length; i++) {
-	  var newYPos = snow_particles[i].yPos + snow_particles[i].speed;
-	  if (newYPos > window.innerHeight) {
-		newYPos = getRandomInt(-100,-10);
-		snow_particles[i].xPos = getRandomInt(0, window.innerWidth);
-		snow_particles[i].speed = Math.random() * (maxSpeed - minSpeed) + minSpeed;
-		snow_particles[i].opacity = Math.random() * (maxOpacity - minOpacity) + minOpacity;
-		snow_particles[i].size = Math.random() * (maxSize - minSize) + minSize;
-		snow_particles[i].image = images[Math.floor(Math.random() * images.length)];
-	  }
-	  snow_particles[i].yPos = newYPos;
-	  ctx.drawImage(snow_particles[i].image, snow_particles[i].xPos, newYPos, snow_particles[i].size, snow_particles[i].size);
-	}
+  var c = document.getElementById(canvasId);
+  var ctx = c.getContext("2d");
+  ctx.clearRect(0, 0, c.width, c.height);
+  for (i = 0; i < snow_particles.length; i++) {
+    var newYPos = snow_particles[i].yPos + snow_particles[i].speed;
+    if (newYPos > window.innerHeight) {
+      newYPos = getRandomInt(-100, -10);
+      snow_particles[i].xPos = getRandomInt(0, window.innerWidth);
+      snow_particles[i].speed =
+        Math.random() * (maxSpeed - minSpeed) + minSpeed;
+      snow_particles[i].opacity =
+        Math.random() * (maxOpacity - minOpacity) + minOpacity;
+      snow_particles[i].size = Math.random() * (maxSize - minSize) + minSize;
+      snow_particles[i].image =
+        images[Math.floor(Math.random() * images.length)];
+    }
+    snow_particles[i].yPos = newYPos;
+    ctx.drawImage(
+      snow_particles[i].image,
+      snow_particles[i].xPos,
+      newYPos,
+      snow_particles[i].size,
+      snow_particles[i].size
+    );
+  }
 }
 
 function InitPoints() {
-	for (i = 0; i < numParticles; i++) {
-		var startX = getRandomInt(0, window.innerWidth);
-		var startY = getRandomInt(0, window.innerHeight);
-		var speed = Math.random() * (maxSpeed - minSpeed) + minSpeed;
-		var opacity = Math.random() * (maxOpacity - minOpacity) + minOpacity;
-		var size = Math.random() * (maxSize - minSize) + minSize;
+  for (i = 0; i < numParticles; i++) {
+    var startX = getRandomInt(0, window.innerWidth);
+    var startY = getRandomInt(0, window.innerHeight);
+    var speed = Math.random() * (maxSpeed - minSpeed) + minSpeed;
+    var opacity = Math.random() * (maxOpacity - minOpacity) + minOpacity;
+    var size = Math.random() * (maxSize - minSize) + minSize;
 
-		snow_particles.push({
-			"xPos": startX,
-			"yPos": startY,
-			"speed": speed,
-			"opacity": opacity,
-			"size": size,
-		});
-	}
+    snow_particles.push({
+      xPos: startX,
+      yPos: startY,
+      speed: speed,
+      opacity: opacity,
+      size: size,
+    });
+  }
 }
 
 function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
